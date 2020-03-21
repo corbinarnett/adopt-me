99fee0efc571c4f22266578b9b9877bdd522fd50
corbinarnett <corbin.c.arnett@gmail.com>
2020-03-20 18:53:26 -0700

    use props in pet component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-const Pet = () => {
+const Pet = ({ name, animal, breed}) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Hank"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Frenchie"),
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
 };
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Hank",
+      animal: "Dog",
+      breed: "Frenchie",
+    }),
+    React.createElement(Pet, {
+      name: "Spot",
+      animal: "Dog",
+      breed: "Australian Shepherd",
+    }),
+    React.createElement(Pet, { name: "Cash", animal: "Cat", breed: "Siamese" }),
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
