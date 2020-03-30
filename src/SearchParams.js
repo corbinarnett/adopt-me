2e159bd4e18016afe99b4037613b79d0d8f0444b
corbinarnett <corbin.c.arnett@gmail.com>
2020-03-29 19:35:55 -0700

    import animals array of strings, created dropdown list

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
-
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
-    <h1>{location}</h1>
+      <h1>{location}</h1>
       <form>
         <label htmlFor="location">
           Location
@@ -16,6 +17,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
